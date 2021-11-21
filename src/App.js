f80da1f0518abb6349bbd14649386d23755f1965
Jeffrey Wilcox <jwilcox@twitter.com>
2021-11-21 10:45:31 -0800

    Including jquery, simple fetch request to faraway api via codetabs proxy service to side step CORS error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
+
+const API_KEY = '123';
+const UUID = 'a19bd368-5bfa-4a64-9aa6-a88ef8c2e135';
+const ROOT_URL = 'https://api.faraway.cam/v1/';
+const CORS_PROXY_URL_PREFIX = 'https://api.codetabs.com/v1/proxy/?quest=';
+
+
+
+const JSON_DATA = grabJsonFromApiAsync();
+// need to add a promise for handling photo url loading...
+
+//console.log(JSON_DATA.photourl);
+
+
 
 function App() {
   return (
@@ -21,6 +36,26 @@ function App() {
       </header>
     </div>
   );
+}
+
+function grabJsonFromApiAsync() {
+  return fetch('https://api.codetabs.com/v1/proxy/?quest=https://api.faraway.cam/v1/uuid/a19bd368-5bfa-4a64-9aa6-a88ef8c2e135?API_KEY=123')
+  .then((response) => response.json())
+  .then((responseJson) => {
+    console.log(responseJson);// lets look...
+    console.log(responseJson.photourlhi);
+/*
+    // time to start preloading the image data...
+    //photourl
+    this.setState({
+      content: responseJson.photourlhi,
+    });
+*/
+    return responseJson;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 }
 
 export default App;
