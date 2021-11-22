80f0ea6835c0f6ba14aa68db29d623694487743d
Jeffrey Wilcox <jwilcox@twitter.com>
2021-11-22 03:13:28 -0800

    Use componentDidMount to handle api fetch with image url stored in app state

--- src/App.js
+++ src/App.js
@@ -2,56 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import $ from 'jquery';
-
+/*
 const API_KEY = '123';
 const UUID = 'a19bd368-5bfa-4a64-9aa6-a88ef8c2e135';
 const ROOT_URL = 'https://api.faraway.cam/v1/';
 const CORS_PROXY_URL_PREFIX = 'https://api.codetabs.com/v1/proxy/?quest=';
-
-const JSON_DATA = grabJsonFromApiAsync();
+*/
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photourlhi: logo
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.codetabs.com/v1/proxy/?quest=https://api.faraway.cam/v1/uuid/a19bd368-5bfa-4a64-9aa6-a88ef8c2e135?API_KEY=123')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson.photourlhi);
+      this.setState({
+        photourlhi: responseJson.photourlhi,
+      });
+
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={this.state.photourlhi} className={(this.state.photourlhi === logo) ? "App-logo" : "App-view"} alt="logo" />
+          <p>Welcome.</p>
         </header>
       </div>
     );
   }
-}
-
-function grabJsonFromApiAsync() {
-  return fetch('https://api.codetabs.com/v1/proxy/?quest=https://api.faraway.cam/v1/uuid/a19bd368-5bfa-4a64-9aa6-a88ef8c2e135?API_KEY=123')
-  .then((response) => response.json())
-  .then((responseJson) => {
-    //console.log(responseJson);// lets look...
-    console.log(responseJson.photourlhi);
-/*
-    // time to start preloading the image data...
-    //photourl
-    this.setState({
-      content: responseJson.photourlhi,
-    });
-*/
-    return responseJson;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
 }
 
 export default App;
