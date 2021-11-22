c39a9f6c382c579b0f3803c88c342d5fbff848bf
Jeffrey Wilcox <jwilcox@twitter.com>
2021-11-22 01:34:55 -0800

    converting App.js into class syntax

--- src/App.js
+++ src/App.js
@@ -8,41 +8,37 @@ const UUID = 'a19bd368-5bfa-4a64-9aa6-a88ef8c2e135';
 const ROOT_URL = 'https://api.faraway.cam/v1/';
 const CORS_PROXY_URL_PREFIX = 'https://api.codetabs.com/v1/proxy/?quest=';
 
-
-
 const JSON_DATA = grabJsonFromApiAsync();
-// need to add a promise for handling photo url loading...
-
-//console.log(JSON_DATA.photourl);
 
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 function grabJsonFromApiAsync() {
   return fetch('https://api.codetabs.com/v1/proxy/?quest=https://api.faraway.cam/v1/uuid/a19bd368-5bfa-4a64-9aa6-a88ef8c2e135?API_KEY=123')
   .then((response) => response.json())
   .then((responseJson) => {
-    console.log(responseJson);// lets look...
+    //console.log(responseJson);// lets look...
     console.log(responseJson.photourlhi);
 /*
     // time to start preloading the image data...
